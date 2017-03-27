34d07dd2e8f8e7d0d9a7b0233a39ba7fc2421840
Trevor Larson <trevor.larson@comcast.net>
2017-03-27 01:52:56 -0700

    Add project files.

---
+++ Program2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+
+        struct sequence
+        {
+            public string name;
+            public string sequenceString;
+        }
+
+        //Sequence Variables
+        static sequence s1;
+
+        static string[] alphabet;
+
+
+        static string inputFile = "";
+        static string alphabetFile = "";
+
+        static void Main(string[] args)
+        {
+            inputFile = args[0];
+            alphabetFile = args[1];
+
+            populateSequences();
+
+            populateAlphabet();
+
+            Console.WriteLine("Finished Execution");
+        }
+
+        static void populateSequences()
+        {
+            StreamReader FastaReader = new StreamReader("./" + inputFile);
+            string line;
+
+            line = FastaReader.ReadLine().Trim().ToLower();
+            if (line.StartsWith(">"))
+            {
+                s1.name = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)[0].Substring(1);
+                s1.sequenceString = "";
+            }
+
+            while ((line = FastaReader.ReadLine()) != null)
+            {
+                line = line.Trim().ToLower();
+                if (line.StartsWith(">"))
+                    break;
+                s1.sequenceString += line;
+            }
+
+        }
+
+        static void populateAlphabet()
+        {
+            StreamReader alphabetReader = new StreamReader("./" + alphabetFile);
+            string line;
+
+            line = alphabetReader.ReadLine().Trim().ToLower();
+            alphabet = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        }
+    }
+}
